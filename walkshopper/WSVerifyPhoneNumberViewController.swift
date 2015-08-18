78b514e740edea2e2fd0bcf6b0d1ed618bb6d352
tennysondy <tennyson_dy@sina.com>
2015-08-19 00:14:57 +0800

    增加Alamofire

--- walkshopper/WSVerifyPhoneNumberViewController.swift
+++ walkshopper/WSVerifyPhoneNumberViewController.swift
@@ -18,7 +18,6 @@ class WSVerifyPhoneNumberViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 }
